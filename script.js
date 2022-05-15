4b23092047729d46d3bec09015ffd0d3c5c88775
nattyKhoe <natasha.kusuma@outlook.com>
2022-05-15 16:24:19 +1000

    Finished with the task

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript";
-document.querySelector('body').appendChild(h2);
+//Sourced from W3school.com//
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
